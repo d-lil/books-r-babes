1436ececbe4fd0f792e3d22f9496b93058e104d6
Danny <dahneel@gmail.com>
2023-05-25 18:00:28 -0600

    Cleaned up a few things

--- server/server.js
+++ server/server.js
@@ -23,13 +23,13 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 // app.use(routes);
 const startApolloServer = async () => {
